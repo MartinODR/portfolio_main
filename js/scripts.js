24e43c3e8518fe7745faaa051ba076a0c82aee4e
MartinODR <martinsteinturm@gmail.com>
2024-03-14 00:10:44 +0100

    main

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ const toggleText = document.getElementById("toggle-text");
 
 toggleTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark");
-    if(toggleIcon.scr.includes("moon.svg")){
+    if(toggleIcon.src.includes("moon.svg")) {
         toggleIcon.src="assets/icons/sun.svg";
-        toggleText.textContent="Light Mode";
+        toggleText.textContent= "Light Mode";
     }else{
         toggleIcon.src="assets/icons/moon.svg";
         toggleText.textContent="Dark Mode";
